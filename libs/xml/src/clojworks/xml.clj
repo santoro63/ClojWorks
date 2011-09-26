ef0f33f9664cc7c758b8e48f85aa715d7b5b0b7e
Alex S <santoro63@yahoo.com>
2011-09-26 11:24:03 -0700

    slightly simpler version of the code

--- libs/xml/src/clojworks/xml.clj
+++ libs/xml/src/clojworks/xml.clj
@@ -18,28 +18,27 @@
        (if (not (empty? attribs)) (str " " (attrib-str attribs)))))
 
 (defn single-line [indent el]
-  (let [ el-name (el 0)
-	attribs (el 1)
-	text (el 2) ]
+  (let [ el-name (get el 0)
+	attribs (get el 1)
+	text (get el 2) ]
     (str (space indent)
 	 (open-el el-name attribs) 
 	 (if (nil? text) "/>\n" (str ">" text "</" el-name ">\n")))))
 
-(defn el-to-string
-  ( [indent el-name]
-      (single-line indent [el-name nil nil] ))
-  ( [indent el-name attribs ]
-      (single-line indent [el-name  attribs nil nil] ))
-  ( [indent el-name attribs text]
-      (single-line indent [el-name attribs text]))
-  ( [indent el-name attribs text & children]
-    (let [ prefix (space indent) ]
-      (if (empty? children)
-        (single-line indent [el-name attribs text] )
-        (str prefix (open-el el-name attribs) ">\n"
-             (str-join "" (map #(apply el-to-string (cons (+ 4 indent) %)) (filter #(not (empty? %)) children)))
-	     prefix "</" el-name ">\n"))))
-)  
+(defn el-to-string [ indent el ]
+  (if (> 3 (count el))
+	 (single-line indent el)
+         (let [ prefix (space indent)
+	       el-name (get el 0)
+	       attribs (get el 1)
+	       text (get el 2)
+	       children (subvec el 3) ]
+	   (if (empty? children)
+	     (single-line indent [el-name attribs text] )
+	     (str prefix (open-el el-name attribs) ">\n"
+		  (str-join "" (map #(el-to-string (+ 4 indent) %) (filter #(not (empty? %)) children)))
+		  prefix "</" el-name ">\n"))))
+)
 ;; pretty prints the xml data to a string according to the following
 ;; rules:
 ;;    (1) elements without children or text:
@@ -63,4 +62,4 @@
 ;; xml-data is expected to be a vector in the following format:
 ;;    [ el_name attribMapOrNil textOrNil [ child1 ] [ child2 ] ...  ]
 (defn pretty-print-xml [ xml-data ]
-  (apply el-to-string (cons 0 xml-data)))
+  (apply el-to-string 0 xml-data))
